ffda6b504ca57359ae95a9d9ad8c8b906953c782
Andreas Drotth <drotth@hotmail.com>
2021-09-24 15:24:02 +0200

    Created building script and all building prefabs

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BuildingType
+{
+    Base,
+    greenhouse,
+    Mine,
+    SolarPanel
+}
+
+public enum ResourceType
+{
+    Food,
+    Metal,
+    Oxygen,
+    Energy
+}
+
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public BuildingType type;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Production")]
+    public bool doesProduceResource;
+    public ResourceType productionResource;
+    public int productionResourcePerTrun;
+
+    [Header("Maintenance")]
+    public bool hasMaintenaceCost;
+    public ResourceType maintenanceResource;
+    public int maintenanceResourcePerTurn;
 }
